3cf01be65a4dbaf76db24eec60c95f11a87caf03
replayzor <oltean.ionut1989@gmail.com>
2023-01-18 13:05:45 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import ImageList from "./components/ImageList";
+import ImageShow from "./components/ImageShow";
+import SearchBar from "./components/SearchBar";
+
+function App() {
+	return (
+		<div>
+			<SearchBar />
+			<ImageList />
+			<ImageShow />
+		</div>
+	);
+}
+
+export default App;
