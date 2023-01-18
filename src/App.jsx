8d336cd0ab6ce73b7130e9c53dcc11384c8e7d88
replayzor <oltean.ionut1989@gmail.com>
2023-01-18 18:06:33 +0200

    added a handleSubmit function and pass it to SearchBar component

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,13 @@ import ImageShow from "./components/ImageShow";
 import SearchBar from "./components/SearchBar";
 
 function App() {
+	const handleSubmit = (term) => {
+		console.log("Do a search with", term);
+	};
+
 	return (
 		<div>
-			<SearchBar />
-			<ImageList />
-			<ImageShow />
+			<SearchBar handleSubmit={handleSubmit} />
 		</div>
 	);
 }
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,5 +1,25 @@
-const SearchBar = () => {
-	return <div>SearchBar</div>;
+import { useState } from "react";
+
+const SearchBar = ({ handleSubmit }) => {
+	const [term, setTerm] = useState("");
+
+	const handleFormSubmit = (e) => {
+		e.preventDefault();
+
+		handleSubmit("cars");
+	};
+
+	const handleChange = (e) => {
+		setTerm(e.target.value);
+	};
+
+	return (
+		<div>
+			<form onSubmit={handleFormSubmit}>
+				<input onChange={handleChange} value={term} />
+			</form>
+		</div>
+	);
 };
 
 export default SearchBar;
