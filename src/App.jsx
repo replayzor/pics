5709892aa634472afde96f1134f4bc4afe28a8d3
replayzor <oltean.ionut1989@gmail.com>
2023-01-19 12:59:08 +0200

    added searchImages

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,9 @@
-import ImageList from "./components/ImageList";
-import ImageShow from "./components/ImageShow";
 import SearchBar from "./components/SearchBar";
+import searchImages from "./api";
 
 function App() {
 	const handleSubmit = (term) => {
-		console.log("Do a search with", term);
+		searchImages(term);
 	};
 
 	return (
