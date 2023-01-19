790155a96399a4a78024b8e66644cf692606e5ae
replayzor <oltean.ionut1989@gmail.com>
2023-01-19 15:17:31 +0200

    added images state, updated the state pass in the ImageList component

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,21 @@
 import SearchBar from "./components/SearchBar";
+import ImageList from "./components/ImageList";
 import searchImages from "./api";
+import { useState } from "react";
 
 function App() {
-	const handleSubmit = (term) => {
-		searchImages(term);
+	const [images, setImages] = useState([]);
+
+	const handleSubmit = async (term) => {
+		const result = await searchImages(term);
+
+		setImages(result);
 	};
 
 	return (
 		<div>
 			<SearchBar handleSubmit={handleSubmit} />
+			<ImageList images={images} />
 		</div>
 	);
 }
