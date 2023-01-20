02842f9310f27780b9a5f82712361903e8330fc9
replayzor <oltean.ionut1989@gmail.com>
2023-01-20 11:09:06 +0200

    add styles and key

--- src/components/ImageList.jsx
+++ src/components/ImageList.jsx
@@ -1,11 +1,15 @@
+// components
 import ImageShow from "./ImageShow";
+
+// styles
+import "./ImageList.css";
 
 const ImageList = ({ images }) => {
 	const renderedImages = images.map((image) => {
 		return <ImageShow key={image.id} image={image} />;
 	});
 
-	return <div>{renderedImages}</div>;
+	return <div className="image-list">{renderedImages}</div>;
 };
 
 export default ImageList;
